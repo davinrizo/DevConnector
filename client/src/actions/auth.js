de718bdf24f8e9b0bd844decadfc360335c27613
davin <dizqar@gmail.com>
2021-05-04 15:16:11 +0700

    login dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,6 +9,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from './types';
 
 export const loadUser = () => async (dispatch) => {
@@ -87,5 +88,6 @@ export const login = (email, password) => async (dispatch) => {
 
 // logout / clear profile
 export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
